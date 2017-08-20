ee62963b561bfb56a823b8cff075ef6f4ec55625
Mateus <mateus.esteves1994@gmail.com>
2017-08-19 21:40:14 -0300

    Conclusão do APP de navegação.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,15 @@
+import { AboutPage } from './../about/about';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  aboutPage = AboutPage;
 
   constructor(public navCtrl: NavController) {
 
